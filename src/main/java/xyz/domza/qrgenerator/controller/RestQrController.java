62b135b40fa40ba7973d42da66070acbd80814bb
Domza64 <juricicdominik@protonmail.com>
2023-10-28 13:08:51 +0200

    Can download SVG and PNG | new UI

--- src/main/java/xyz/domza/qrgenerator/controller/RestQrController.java
+++ src/main/java/xyz/domza/qrgenerator/controller/RestQrController.java
@@ -1,23 +1,14 @@
 package xyz.domza.qrgenerator.controller;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpStatus;
+import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.javase.QRCode;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.BufferedImageHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.awt.image.BufferedImage;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,22 +16,31 @@ import java.util.Map;
 @RequestMapping("/api")
 public class RestQrController {
 
-    private Map<Integer, BufferedImage> tempMap = new HashMap<>();
+    private final Map<Integer, QRCode> tempMap = new HashMap<>();
 
-    @GetMapping(value = "qrcode/{barcode}", produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<BufferedImage> barbecueEAN13Barcode(@PathVariable("barcode") String code)
-            throws Exception {
-        int id = 0;
+    @GetMapping(value = "qrcode/svg/{qrcode}")
+    public ResponseEntity<byte[]> getSvgQrCode(@PathVariable("qrcode") String code) throws IOException {
+        File svgFile = tempMap.get(Integer.parseInt(code)).svg();
+        byte[] file = Files.readAllBytes(svgFile.toPath());
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content-Type","image/svg+xml");
+        return ResponseEntity.ok()
+                .headers(header)
+                .body(file);
+    }
+
+    @GetMapping(value = "qrcode/{qrcode}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> getPngQrCode(@PathVariable("qrcode") String code) {
         try {
-            id = Integer.parseInt(code);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return ResponseEntity.ok(tempMap.get(id));
+            int id = Integer.parseInt(code);
+            byte[] image = tempMap.get(id).to(ImageType.PNG).stream().toByteArray();
+            return ResponseEntity.ok(image);
+        } catch (Exception ignored) {}
+        return null;
     }
 
     @PostMapping(value = "/getId")
-    public Integer getId(@RequestBody String text) throws Exception {
+    public Integer getId(@RequestBody String text) {
         String result = java.net.URLDecoder.decode(text, StandardCharsets.UTF_8).split("=")[1];
         int id = tempMap.size();
         tempMap.put(id, generateQRCodeImage(result));
@@ -48,17 +48,7 @@ public class RestQrController {
     }
 
 
-    public static BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
-        QRCodeWriter barcodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix =
-                barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
-
-        return MatrixToImageWriter.toBufferedImage(bitMatrix);
-    }
-
-
-    @Bean
-    public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
-        return new BufferedImageHttpMessageConverter();
+    public static QRCode generateQRCodeImage(String barcodeText) {
+        return QRCode.from(barcodeText).withSize(200, 200);
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,5 +2,7 @@ function loadQrCode() {
     $.post("/api/getId", { text:$('#input').val() }, function (data) {
         var image = $('<img src="/api/qrcode/' + data + '"></img>');
         $('#qr-container').html('').append(image);
+        $('#download-svg').attr('href', '/api/qrcode/svg/' + data);
+        $('#download-png').attr('href', '/api/qrcode/' + data);
     });
 }
